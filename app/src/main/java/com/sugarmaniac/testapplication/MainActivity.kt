7847a8fc957428095370b617e6a34da5b568de52
sugarmaniac <oseker@etu.edu.tr>
2022-03-27 00:16:25 +0300

    Fetch data from api moved from list fragment to activity

--- app/src/main/java/com/sugarmaniac/testapplication/MainActivity.kt
+++ app/src/main/java/com/sugarmaniac/testapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.sugarmaniac.testapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.sugarmaniac.testapplication.viewModel.BaseViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModel : BaseViewModel by viewModels()
+        viewModel.fetchData()
     }
+
+    override fun onBackPressed() {
+        findNavController(R.id.fragmentContainerView).navigateUp()
+    }
+
+
 }
